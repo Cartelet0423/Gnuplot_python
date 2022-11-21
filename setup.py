0cd5df48213eaaccc85b2e8d05a8cc22e1c7f6c0
Cartelet0423 <55804871+Cartelet0423@users.noreply.github.com>
2022-11-22 03:55:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-"""Minimal setup file for tasks project."""
-
 from setuptools import setup, find_packages
 
 setup(
     name="Gnuplot",
     version="0.1.0",
-    license="proprietary",
+    license="MIT",
     description="The Easy-to-use PyGnuplot Wrapper.",
     author="Cartelet0423",
     author_email="",
